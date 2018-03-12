ef98916b9679ecab4b914080278ce994a1dccaf8
Mark Glagola <mark.glagola@gmail.com>
2018-03-12 12:42:46 -0500

    Added package version check

--- index.js
+++ index.js
@@ -5,9 +5,11 @@
 const Promise = require('bluebird');
 global.Promise = Promise;
 
+const pkg = require('./package.json');
 const rp = require('request-promise');
 const meow = require('meow');
 const chalk = require('chalk');
+const { logVersionCheck } = require('validate-package-version');
 const fs = Promise.promisifyAll(require('fs'));
 
 const cli = meow(`
@@ -19,6 +21,21 @@ const cli = meow(`
     $ genignore android
     $ genignore node
 `);
+
+const isNil = (value) => value === null || value === undefined;
+
+function isEmpty (value) {
+    if (isNil(value)) {
+        return true;
+    }
+    switch (typeof value) {
+    case 'string':
+    case 'object':
+        return Object.keys(value).length === 0;
+    default:
+        return false;
+    }
+}
 
 function flatten (array) {
     return [].concat.apply([], array);
@@ -55,6 +72,23 @@ async function gen (names) {
     const gitignore = gitignores.filter(x => x).join('');
     await fs.writeFileAsync('.gitignore', gitignore);
     console.log(chalk.green('Generated .gitignore!'));
+    return true;
 }
 
-gen(cli.input);
+(async function () {
+    try {
+        const input = cli.input;
+        if (isEmpty(input)) {
+            console.log(chalk.red('Please specify at least one gitignore file name to fetch.'), 'See --help for usage details');
+            process.exit(1);
+        }
+        await logVersionCheck(pkg);
+        const success = true // await gen(input);
+        // const success = await gen(input);
+        return process.exit(success ? 0 : 1);
+    } catch (error) {
+        console.log(chalk.red(error.message));
+        console.error(error.stack);
+        return process.exit(1);
+    }
+})();
